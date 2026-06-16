c62e7aa3eae12bbc0110d4d9183fc1404435c551
Filippo Graziano <filippograziano13@gmail.com>
2026-06-16 13:59:22 +0200

    complete snack-1

--- ex-js-array-methods/snack-1/script.js
+++ ex-js-array-methods/snack-1/script.js
@@ -1,4 +1,6 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
+
+names.forEach((element) => console.log(element))
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
