a20d9fc4185b70da8e577edd76f0f233a33ba389
Filippo Graziano <filippograziano13@gmail.com>
2026-06-16 14:13:34 +0200

    complete snack-7

--- ex-js-array-methods/snack-7/script.js
+++ ex-js-array-methods/snack-7/script.js
@@ -20,4 +20,6 @@ const students = [
 ];
 
 // Recupera i dati dello studente con id 2
+
+console.log(students.find((el) => el.id === 2))
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
